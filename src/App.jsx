5d0a9933efc66de6dafdfaa2c94793fbcbbc3039
saraswati_kumar <kusaraswati45@gmail.com>
2025-08-07 11:56:02 +0530

    ssss

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
+import About from './components/About'
+import RoboticsSection from './components/RoboticsSection '
+import ServicesSection from './components/ServicesSection'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-        
+       <Navbar /> 
+       <About />  
+       <RoboticsSection />
+       <ServicesSection />
     </>
   )
 }
