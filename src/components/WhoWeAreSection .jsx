125783d96f85d684938879271bf5115ed31b18cf
saraswati_kumar <kusaraswati45@gmail.com>
2025-08-07 16:29:55 +0530

    saras

--- src/components/WhoWeAreSection .jsx
+++ src/components/WhoWeAreSection .jsx
@@ -31,7 +31,7 @@ const WhoWeAreSection = () => {
             {/* Left Content */}
             <div className="md:w-1/2 mb-10 md:mb-0 pe-25">
                 <p className="text-orange-500 font-semibold uppercase mb-2">Who We Are</p>
-                <h2 className="text-3xl md:text-4xl font-bold bg-[#04394e] p-3 rounded-lg text-white leading-tight mb-6">
+                <h2 className="text-lg md:text-4xl font-bold bg-[#04394e] p-3 rounded-lg text-white leading-tight mb-6">
                     {typedText}
                     
                 </h2>
