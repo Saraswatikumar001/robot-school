f67aab468b294f126a10fd26f19d7d65ee8cf7a2
saraswati_kumar <kusaraswati45@gmail.com>
2025-09-01 17:02:17 +0530

    saras

--- src/components/FeaturedCourses.jsx
+++ src/components/FeaturedCourses.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CourseImage from "../assets/img/four-kid.jpg";
 import Slider from "react-slick";
+import { div } from "framer-motion/client";
 
 const courses = [
     {
@@ -61,7 +62,7 @@ const FeaturedCourses = () => {
             {
                 breakpoint: 1024, // tablets
                 settings: {
-                    slidesToShow: 1
+                    slidesToShow: 3
                 }
             },
             {
@@ -84,44 +85,46 @@ const FeaturedCourses = () => {
         <section className="bg-[#f2f4ff] py-12 px-10">
             <div className="max-w-6xl mx-auto px-4">
                 <h2 className="text-5xl font-bold text-center mb-10 text-[#04394e]">Featured Courses</h2>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+                <div className="p-5">
                     <Slider {...settings}>
                         {courses.map((course, idx) => (
-                            <div
-                                key={idx}
-                                className="bg-white rounded-lg shadow-lg overflow-hidden relative"
-                            >
-                                {/* Price Tag */}
-                                <div className="absolute left-[-1%] top-[33%] bg-[#04394e] text-white  border-4 border-white font-bold px-3 py-1 lg:rounded-full">
-                                    {course.price}
-                                </div>
-
-                                {/* Image */}
-                                <img
-                                    src={CourseImage}
-                                    alt={course.title}
-                                    className="w-full h-48 object-cover"
-                                />
-
-                                {/* Content */}
-                                <div className="p-5">
-                                    <p className="text-xs font-bold text-orange-500 tracking-widest uppercase mb-2">
-                                        {course.category}
-                                    </p>
-                                    <h3 className="font-semibold text-lg mb-2">{course.title}</h3>
-
-                                    {/* Rating */}
-                                    <div className="flex items-center text-sm mb-3">
-                                        <span>({course.rating})</span>
-                                        <span className="text-orange-500 ml-1">★★★★★</span>
+                            <div key={idx} className="lg:px-2">
+                                <div
+                                    
+                                    className="bg-white rounded-lg shadow-lg overflow-hidden relative me-3"
+                                >
+                                    {/* Price Tag */}
+                                    <div className="absolute left-[-1%] top-[33%] bg-[#04394e] text-white  border-4 border-white font-bold px-3 py-1 lg:rounded-full">
+                                        {course.price}
                                     </div>
 
-                                    <p className="text-gray-600 text-sm mb-4">{course.description}</p>
+                                    {/* Image */}
+                                    <img
+                                        src={CourseImage}
+                                        alt={course.title}
+                                        className="w-full h-48 object-cover"
+                                    />
 
-                                    {/* Button */}
-                                    <button className="bg-[#f26b0c] hover:bg-orange-600 text-white border-4 border-white  font-semibold px-6 py-2 rounded-full shadow-md transition-all duration-300">
-                                        LEARN MORE
-                                    </button>
+                                    {/* Content */}
+                                    <div className="p-5">
+                                        <p className="text-xs font-bold text-orange-500 tracking-widest uppercase mb-2">
+                                            {course.category}
+                                        </p>
+                                        <h3 className="font-semibold text-lg mb-2">{course.title}</h3>
+
+                                        {/* Rating */}
+                                        <div className="flex items-center text-sm mb-3">
+                                            <span>({course.rating})</span>
+                                            <span className="text-orange-500 ml-1">★★★★★</span>
+                                        </div>
+
+                                        <p className="text-gray-600 text-sm mb-4">{course.description}</p>
+
+                                        {/* Button */}
+                                        <button className="bg-[#f26b0c] hover:bg-orange-600 text-white border-4 border-white  font-semibold px-6 py-2 rounded-full shadow-md transition-all duration-300">
+                                            LEARN MORE
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         ))}
