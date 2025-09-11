d86a395b44a1a0428542cd2d93aae15ee2f6c371
saraswati_kumar <kusaraswati45@gmail.com>
2025-09-11 15:29:16 +0530

    saras

--- src/components/TestimonialSection;.jsx
+++ src/components/TestimonialSection;.jsx
@@ -172,7 +172,7 @@ const TestimonialSection = () => {
     infinite: true,
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 1,
+    slidesToScroll: 3,
     autoplay: true,
     autoplaySpeed: 3000,
     responsive: [
