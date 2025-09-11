ca96db2c7ac43e5b1e76328cbe854a3a2c9cdff8
saraswati_kumar <kusaraswati45@gmail.com>
2025-09-11 13:00:24 +0530

    saras

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
             <TestimonialSection />
             <FeaturedCourses />
             <FAQContactSection />
-            <EnrollNow />
+           
         </>
     )
 }
